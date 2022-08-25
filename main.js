02a35fc6c2234b0256a0e381211e1cb75236ca35
Jeremias Ramos <Exzed13@hotmail.com>
2022-08-24 21:10:06 -0300

    Completado

---
+++ main.js
@@ -0,0 +1,69 @@
+//FUNCTION FOR
+
+// for (let i = 1; i <=4; i++) {
+//     console.log(`iteracion ${i}`);
+// }
+// console.log('Fin del ciclo');
+
+
+//FOR INVERSO
+
+// for (let i = 4; i >=1; i--) {
+//     console.log(`iteracion ${i}`);
+// }
+// console.log('Fin del ciclo');
+
+
+//BUCLE ANIDADO
+
+// for (let i = 1; i <= 4; i++) {
+//     console.log(`Empieza iteracion ${i}`);
+//     for (let j = 0; j < 4; j++) {
+
+//         console.log(j);
+//     }
+// }
+// console.log(`Fin del ciclo`)
+
+
+//FUNCTION WHILE
+
+// let i = 1 
+// while (i < 11) {
+//     console.log(`Iteracion ${i}`);
+//     i++;
+// }
+
+// function bucleWhile(num) {
+//     let i = 0;
+//     while(i < num) {
+//         console.log(i);
+//         i++;
+//     }
+// }
+
+// bucleWhile(11)
+
+
+//FUNCTION DOWHILE
+
+// function doWhile(num) {
+//     let i = 0;
+//     do {
+//         console.log(i);
+//         i++;
+//     } while(i< num);
+// }
+//  doWhile(11);
+
+
+//__________________________
+
+
+let j = 5;
+
+do {
+    console.log(`Iteracion ${j}`);
+    j++;
+
+} while (j <=10);
